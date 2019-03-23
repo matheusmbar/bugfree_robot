c8fb075b71c5a55fc73be8089970f7f1fbaadb4a
Matheus Marques <matheus@agrosmart.com.br>
2019-03-23 16:58:55 -0300

    added multiply library with tests

---
+++ multiply.c
@@ -0,0 +1,7 @@
+
+#include "multiply.h"
+
+
+int16_t multiply (int8_t a, int8_t b){
+	return (int16_t)(a*b);
+}
---
+++ multiply.h
@@ -0,0 +1,18 @@
+#ifndef MULTIPLY_H
+#define MULTIPLY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+int16_t multiply (int8_t a, int8_t b);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
